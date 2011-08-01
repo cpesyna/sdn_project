0b0b84e50a22a622f56e82f1f04f8994b7421dcb
Colin Pesyna <cpesyna@gmail.com>
2011-08-01 15:51:02 -0500

    A basic working prototype: gets user ids, types, and thread names

--- sdn_proj.py
+++ sdn_proj.py
@@ -1,13 +1,67 @@
 #!/usr/bin/python
 
+import Queue
 import threading
 from os import path, makedirs
 from sys import argv
+import re
 import urllib2
 import BeautifulSoup
-from nltk import clean_html
 
-BASE_URL = "http://forums.studentdoctor.net/"
+queue = Queue.Queue()
+output_dirname = "./data/output/"
+
+class Scraper(threading.Thread):
+    """Threaded SDN forum scraper"""
+
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def extract(self, forum_id):
+        BASE_URL = "http://forums.studentdoctor.net/showthread.php?t=" + forum_id + "&page="
+        url = urllib2.urlopen(BASE_URL + "1")
+        soup = BeautifulSoup.BeautifulSoup(url.read())
+        self.school = soup.find("title").next.extract()
+        self.school = self.school.split(" ")
+        self.school = " ".join(self.school[2:self.school.index("|")]).replace(" Application Thread", "")
+        try:
+            page_count = int(soup.find("td", {"class":"vbmenu_control", "style":"font-weight:normal"}).next.extract().split(" ")[-1])
+        except AttributeError:
+            page_count = 1
+        print "Started " + self.school
+
+        self.users = []
+        self.statuses = []
+        for i in xrange(1, page_count + 1):
+            url = urllib2.urlopen(BASE_URL + str(i))
+            soup = BeautifulSoup.BeautifulSoup(url.read())
+            self.users += [item.next.extract() for item in soup.findAll("a", {"title": "You must be a registered member to view member names."})]
+            for item in soup.findAll("table", {"id" : re.compile("post[0-9]*")}):
+                try:
+                    self.statuses.append(item.find("a" , {"href": re.compile("/memberlist.php")}).next.extract())
+                except AttributeError:
+                    self.statuses.append("Unknown")
+
+    def export(self):
+        outfile = open(output_dirname + self.school, "w")
+
+        for item in zip(self.users, self.statuses):
+            outfile.write(",".join(list(item) + [self.school]))
+            outfile.write("\n")
+
+        outfile.close()
+
+
+    def run(self):
+        while True:
+            self.users = []
+            self.statuses = []
+            index = self.queue.get()
+            self.extract(index)
+            self.export()
+
+            self.queue.task_done()
 
 def main():
     try:
@@ -19,7 +73,17 @@ def main():
         print "Creating output directory: %s" % output_dirname
         makedirs(output_dirname)
 
-    O
+    num_threads = 10
+    for i in xrange(num_threads):
+        t = Scraper(queue)
+        t.setDaemon(True)
+        t.start()
+
+    for line in input_file:
+        queue.put(line.strip())
+
+    queue.join()
+
 
 
 if __name__ == '__main__':

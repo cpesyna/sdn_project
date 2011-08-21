deebd92f4204dc6c922147e5eb2c9577e6bfc9cc
Colin Pesyna <cpesyna@gmail.com>
2011-08-21 09:10:27 -0500

    Added threadding to sdn_proj.py

--- sdn_proj.py
+++ sdn_proj.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
 
-import Queue
-import threading
+from itertools import izip
+from nltk import clean_html
 from os import path, makedirs
 from sys import argv
-import re
-import urllib2
+from time import mktime, strptime
+import datetime
 import BeautifulSoup
+import Queue
+import re
+import threading
+import urllib2
 
 queue = Queue.Queue()
 output_dirname = "./data/output/"
+today = datetime.datetime.today()
+yesterday = today - datetime.timedelta(1)
+today = today.strftime('%m-%d-%Y')
+print today
+yesterday = yesterday.strftime('%m-%d-%Y')
+print yesterday
 
 class Scraper(threading.Thread):
     """Threaded SDN forum scraper"""
@@ -35,20 +45,42 @@ class Scraper(threading.Thread):
 
         self.users = []
         self.statuses = []
+        self.timestamps = []
         for i in xrange(1, page_count + 1):
             url = urllib2.urlopen(BASE_URL + str(i))
             soup = BeautifulSoup.BeautifulSoup(url.read())
             self.users += [item.next.extract() for item in soup.findAll("a", {"title": "You must be a registered member to view member names."})]
+
+            ts = soup.findAll("td", {"class" : "thead"})
+            ts = [item for item in ts if "<!-- status icon and date -->" in str(item)]
+            ts = [clean_html(i.extract().renderContents()).replace(",", "") for i in ts]
+            self.timestamps += ts
+
             for item in soup.findAll("table", {"id" : re.compile("post[0-9]*")}):
                 try:
                     self.statuses.append(item.find("a" , {"href": re.compile("/memberlist.php")}).next.extract())
                 except AttributeError:
                     self.statuses.append("Unknown")
 
+        self.epochtime = []
+        for item in self.timestamps:
+            if "Yesterday" in item:
+                item.replace("Yesterday", yesterday)
+            elif "Today" in item:
+                item.replace("Today", today)
+
+            try:
+                self.epochtime.append(str(mktime(strptime(item, '%m-%d-%Y %I:%M %p'))))
+            except ValueError:
+                self.epochtime.append("NA")
+
     def export(self):
         outfile = open(output_dirname + self.school, "w")
 
-        for item in zip(self.users, self.statuses):
+        if len(self.users) != len(self.timestamps):
+            print self.school + "has a timestamp parsing problem"
+
+        for item in izip(self.users, self.statuses, self.timestamps, self.epochtime):
             outfile.write(",".join(list(item) + [self.school]))
             outfile.write("\n")
 
